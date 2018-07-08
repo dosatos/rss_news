4f731419fd83795be58bdfd429c3ae22451e0e51
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-07-08 15:16:29 +0100

    source views authenticated/unauthenticated tests are added

--- feeds/tests/test_views.py
+++ feeds/tests/test_views.py
@@ -1,11 +1,31 @@
+import pytest
+from mixer.backend.django import mixer
 from django.test import RequestFactory
 from django.urls import reverse, resolve
+from django.contrib.auth.models import User, AnonymousUser
 from feeds.views import sources
 
+
+@pytest.mark.django_db
 class TestViews:
 
-    def test_source_view(self):
+    def test_source_view_authenticated(self):
         path = reverse('sources')
         request = RequestFactory().get(path)
+        request.user = mixer.blend(User)
         response = sources(request)
         assert response.status_code == 200
+
+    def test_source_view_unauthenticated(self):
+        path = reverse('sources')
+        request = RequestFactory().get(path)
+        request.user = AnonymousUser()
+        response = sources(request)
+        assert response.status_code == 200
+
+    # def test_source_template_added(self):
+    #     path = reverse('sources')
+    #     request = RequestFactory().get(path)
+    #     response = sources(request)
+    #     assert 'source.html' in [t.name for t in response.templates]
+
