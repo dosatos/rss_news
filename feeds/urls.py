d4ad0d795d9104644a4ffc508d9c11604c009cc2
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-07-08 13:52:20 +0100

    urls corrected

--- feeds/urls.py
+++ feeds/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.feeds, name='feeds'),
-    path('/sources', views.sources, name='sources'),
+    path('', views.feeds, name='feeds'),
+    path('sources/', views.sources, name='sources'),
 ]
