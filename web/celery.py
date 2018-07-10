8de02da2403909a630855f07592dc75b0f4bff94
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-07-10 12:22:21 +0100

    celery beat is up and running automatically now

--- web/celery.py
+++ web/celery.py
@@ -7,7 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
 
 app = Celery('web', broker='redis://redis:6379/0')
 
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
